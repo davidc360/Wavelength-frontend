44401282457171c84abee95d4a38cdec8e6f6af4
David <ddavado@gmail.com>
2020-10-07 19:13:14 -0400

    create room now generates id from front end (temproray workaround)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react'
 import styles from "./Home.module.sass"
 
 import axios from 'axios'
+import shortid from 'shortid'
 
 // const CREATE_ENDPOINT = 'http://50.116.0.53/create'
 const CREATE_ENDPOINT = 'http://127.0.0.1:8000/'
@@ -14,14 +15,15 @@ export default function () {
     }
     
     function createToken() {
-        const config = {
-            headers: {'Access-Control-Allow-Origin': '*'}
-        }
-        axios.get(CREATE_ENDPOINT, config).then(resp => {
-            const token = resp.data
-            console.log(resp.data);
-            window.open("/" + token, "_self") 
-        })
+        // const config = {
+        //     headers: {'Access-Control-Allow-Origin': '*'}
+        // }
+        // axios.get(CREATE_ENDPOINT, config).then(resp => {
+        //     const token = resp.data
+        //     console.log(resp.data);
+        //     window.open("/" + token, "_self") 
+        // })
+        return shortid.generate()
     }
     
     return(
